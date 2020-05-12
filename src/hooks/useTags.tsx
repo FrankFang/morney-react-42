4476c2cf1d0d2b84cf295867584515552e0fb43c
frankfang <frankfang1990@gmail.com>
2020-05-12 21:55:07 +0800

    add record

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef, useState} from 'react';
 import {createId} from 'lib/createId';
-import {useUpdate} from './hooks/useUpdate';
+import {useUpdate} from './useUpdate';
 
 const useTags = () => { // 封装一个自定义 Hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,7 @@ import {TagsSection} from './Money/TagsSection';
 import {CategorySection} from './Money/CategorySection';
 import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
+import {useRecords} from '../hooks/useRecords';
 
 const MyLayout = styled(Layout)`
   display:flex;
@@ -13,27 +14,38 @@ const MyLayout = styled(Layout)`
 
 type Category = '-' | '+'
 
+const defaultFormData = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+};
+
 function Money() {
-  const [selected, setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  });
+  console.log('Money 执行了');
+  const [selected, setSelected] = useState(defaultFormData);
+  const {records, addRecord} = useRecords();
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({...selected, ...obj});
   };
+  const submit = () => {
+    addRecord(selected);
+    alert('保存成功');
+    setSelected(defaultFormData);
+  };
   return (
     <MyLayout>
+      {JSON.stringify(selected)}
+      <hr/>
       <TagsSection value={selected.tagIds}
-                   onChange={tagIds => onChange({tagIds})}/>
+        onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
-                   onChange={note => onChange({note})}/>
+        onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={category => onChange({category})}/>
+        onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={amount => onChange({amount})}
-                        onOk={() => {}}
+        onChange={amount => onChange({amount})}
+        onOk={submit}
       />
     </MyLayout>
   );
